c71832c816cc28fb8593b12b5890a8cb23dc1260
Mauro Assis <assismauro@hotmail.com>
2016-08-19 17:18:29 -0300

    2016.08.19

--- lidarutils/testFWF.py
+++ lidarutils/testFWF.py
@@ -1,38 +1,20 @@
 import lasFWF
 import numpy as np
-import peakutils
-from peakutils.plot import plot as pplot
 from matplotlib import pyplot
 
-'''
-centers = (30.5, 72.3)
-x = numpy.linspace(0, 120, 121)
-y = (peakutils.gaussian(x, 5, centers[0], 3) +
-    peakutils.gaussian(x, 7, centers[1], 10) +
-    numpy.random.rand(x.size))
-pyplot.figure(figsize=(10,6))
-pyplot.plot(x, y)
-pyplot.title("Data with noise")
-pyplot.show()
-'''
+# Open FWF LAS and WDP files
+fwf=lasFWF.FWFFile(r'D:\CCST\Data\T_356\NP_T-356_FWF_LAS\NP_T-356_FWF.LAS')
 
-fwf=lasFWF.FWFFile(r'H:\TRANSECTS\T_054\NP_T-054_FWF_LAS\NP_T-054_FWF.LAS')
-
-index=1000
+# Index indicates the index of discrete point to use in GetFWFData
+index=999
+# Returns a numpy arry containing FWF data corresponding to index
 y=fwf.GetFWFData(index)
+# create X axis values to plot
 x=np.linspace(0,len(y),len(y))
-pyplot.figure(figsize=(10,6))
+# plot graphic
+pyplot.figure(figsize=(30,18))
 pyplot.plot(x, y)
 pyplot.title("Full Waveform")
 pyplot.show()
-
-
-#data=fwf.GetFWFData(111)
-
-
-#with open(r'E:\temp\test.csv', 'w+') as csv:
-#    for i in range(0,999):
-#        data=fwf.GetFWFData(i)
-#        np.savetxt(csv,[data],fmt='%2d',delimiter=';',newline='\n')
-
+# close FWF file
 fwf.close()
